78697129e24aa035136ac9025ec6f2439e1bc78c
charris <cam.cooper.harris@gmail.com>
2024-10-26 08:45:12 +1030

    Deleted unnecessary app/validation_api.py module

--- api/__init__.py
+++ api/__init__.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+from api.funcs import *
 from tools.database_meta import metadata
 
 Base = declarative_base(metadata=metadata)
 
+# Imported down here for context building of SQLAlchemy
 from api import models
-from api.funcs import *
+
